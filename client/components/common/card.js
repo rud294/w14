5454e286d7d9b8c0c619cae2a94e3e2787a83743
rud <rud@rud2017>
2020-10-27 18:18:02 +0300

    изменено:      client/components/common/card.js

--- client/components/common/card.js
+++ client/components/common/card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Card = () => {
     return (
-        <div className="flex flex-col justify-center card max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="card__image w-full" src="#" alt="Sunset in the mountains" />
+        <div className="flex flex-col card max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="card__image w-full object-cover h-40" src="#" alt="Product" />
             <div className="px-6 py-4">
-                <div className="card__title fot-bold text-xl mb-2">Title</div>
+                <div className="card__title font-bold text-xl mb-2">Title</div>
                 <div className="flex justify-evenly">
                     <div className="card__price text-gray-700 text-base">card__price</div>
                     <div className="currency text-gray-700 text-base">currency</div>
@@ -15,7 +15,7 @@ const Card = () => {
             <div className="px-6 pt-4 pb-2">
                <button
                     type="button"
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold"
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 text-base"
                     >Add
                 </button>
             </div>
@@ -23,6 +23,6 @@ const Card = () => {
     )  
 }
 
-Card.prototype = {}
 
-export default React.memo(Card)
+
+export default Card
