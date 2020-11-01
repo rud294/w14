9aca6ba1f924df6daa1973a3663b30c3afc90a1b
Yury <y.rudko@gmail.com>
2020-11-02 00:34:41 +0300

    modified:   client/components/main.js 	modified:   client/redux/reducers/goods.js 	modified:   client/redux/reducers/index.js

--- client/components/main.js
+++ client/components/main.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 import Head from './head'
 import Header from './header'
 import Card from './common/card'
@@ -6,16 +8,33 @@ import Currency from './common/currency'
 import Sort from './common/sort'
 
 import OrderCount from './common/order-count'
-
+import { getGoods } from '../redux/reducers/goods'
+ 
 const Main = () => {
+  const listOfGoods = useSelector( (store) => {
+    return store.goods.listOfGoods
+  })
+  const dispatch = useDispatch()
+
+  useEffect ( () => {
+    dispatch(getGoods)
+    return () => {}
+  }, [])
+  console.log(listOfGoods)
   return (
     <div>
       <Head title="hw14" />
       <Header />
-      <Card />
       <Currency />
       <Sort />
       <OrderCount />
+      { listOfGoods.map((item) => { 
+          return (
+          <div key={ item.id }>
+            (<Card title={ item.title } />)
+          </div>)
+          })  }        
+      {/* <Card /> */}
     </div>
   )
 }
--- client/redux/reducers/goods.js
+++ client/redux/reducers/goods.js
@@ -1,11 +1,19 @@
+import axios from 'axios'
+
 const GET_GOODS = 'GET_GOODS'
 
 const initialState = {
     listOfGoods: []
 }
 
-export default = (state = initialState, action) => {
+export default (state = initialState, action) => {
     switch(action.type) {
+        case GET_GOODS: {
+            return {
+                ...state,
+                listOfGoods: action.data
+            }
+        } 
         default:
             return state
     }
@@ -14,6 +22,8 @@ export default = (state = initialState, action) => {
 export function getGoods() {
 
     return (dispatch) => {
-        
+        axios('/api/v1/goods').then (({data}) => {
+            dispatch({type: GET_GOODS, data})
+        })
     }
 }
